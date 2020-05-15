dc2e078a5ce65396052f76416841e76348d190e6
Demyan Rogozhin <Demyan.Rogozhin@gmail.com>
2020-05-15 14:10:50 +0200

    [map-view] fix parseFixPoint function: straight forward implementation

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,17 @@
 /// Function to convert fixedpoint SCALE-encoded to Number
 /// raw: substrate_fixed::types::I32F32 as I64
 export function parseFixPoint (raw) {
-    // FIXME small brain fixpoint to Number conversion
-    let bits = raw.toString(2, 64);
-    console.debug('0b', bits);
-    return parseInt(bits.slice(0, -31), 2);
+  // Fixed interpretation of u32 place values
+  // ... ___ ___ ___ ___ . ___ ___ ___ ___ ...
+  // ...  8   4   2   1    1/2 1/4 1/8 1/16...
+  const bits = raw.toString(2, raw.bitLength());
+  const upperBits = bits.slice(0, -32);
+  const lowerBits = bits.slice(-32);
+  const floatPart = lowerBits.split('').reduce((acc, bit, idx) => {
+    acc = acc + (bit == '1' ? 1/2**(idx+1) : 0);
+    return acc
+  }, 0);
+  const n = parseInt(upperBits, 2) + (raw.negative ? (-1*floatPart) : floatPart);
+  console.debug(raw, ('0b' + upperBits + '.' + lowerBits), n);
+  return n;
 }
