3e37fdde3e7fd7a8db35693b8d2811e0b26d4966
Demyan Rogozhin <Demyan.Rogozhin@gmail.com>
2020-05-20 21:59:38 +0200

    Fix Human readable demurrage rate

--- src/utils.js
+++ src/utils.js
@@ -1,21 +1,52 @@
-/// Function to convert fixedpoint SCALE-encoded to Number
-/// raw: substrate_fixed::types::I32F32 as I64
-/// precision: 0..32 number bits in fractional part to process
-export function parseFixPoint (raw, precision) {
-  // Fixed interpretation of u32 place values
-  // ... ___ ___ ___ ___ . ___ ___ ___ ___ ...
-  // ...  8   4   2   1    1/2 1/4 1/8 1/16...
-  const bits = raw.toString(2, raw.bitLength());
-  const upperBits = bits.slice(0, -32);
-  const lowerBits = bits.slice(-32, precision ? -1 * (32 - precision) : -16);
-  const floatPart = lowerBits.split('').reduce((acc, bit, idx) => {
-    acc = acc + (bit === '1' ? 1 / 2 ** (idx + 1) : 0);
-    return acc;
-  }, 0);
-  const n = parseInt(upperBits, 2) + (raw.negative ? (-1 * floatPart) : floatPart);
-  console.debug(raw, ('0b' + upperBits + '.' + lowerBits), n);
-  return n;
+import assert from 'assert';
+
+/// Function to produce function to convert fixed-point to Number
+///
+/// Fixed interpretation of u<N> place values
+/// ... ___ ___ ___ ___ . ___ ___ ___ ___ ...
+/// ...  8   4   2   1    1/2 1/4 1/8 1/16...
+///
+/// Parameters:
+/// upper: 0..128 - number of bits in decimal part
+/// lower: 0..128 - number of bits in fractional part
+///
+/// Produced function parameters:
+/// raw: substrate_fixed::types::I<upper>F<lower> as I<upper+lower>
+/// precision: 0..lower number bits in fractional part to process
+export function parserFixPoint (upper, lower) {
+  const len = upper + lower;
+  assert(len >= 8, `Bit length can't be less than 8, provided ${len}`);
+  assert(len <= 128, `Bit length can't be bigger than 128, provided ${len}`);
+  assert(!(len & (len - 1)), `Bit length should be power of 2, provided ${len}`);
+
+  return (raw, precision = lower) => {
+    assert(raw.bitLength() === len, 'Bit length is not equal to ' + len);
+    const bits = raw.toString(2, len);
+    const lowerBits = (lower > bits.length
+      ? bits.padStart(lower, '0')
+      : bits).slice(-lower, -1 * (lower - precision) || undefined);
+    const floatPart = lowerBits
+      .split('')
+      .reduce((acc, bit, idx) => {
+        acc = acc + (bit === '1' ? 1 / 2 ** (idx + 1) : 0);
+        return acc;
+      }, 0);
+    const upperBits = bits.slice(0, -lower);
+    const decimalPart = upperBits ? parseInt(upperBits, 2) : 0;
+    return decimalPart + (raw.negative ? -floatPart : floatPart);
+  };
 }
+
+export const parseI4F4 = parserFixPoint(4, 4);
+
+export const parseI8F8 = parserFixPoint(8, 8);
+
+export const parseI16F16 = parserFixPoint(16, 16);
+
+export const parseI32F32 = parserFixPoint(32, 32);
+
+export const parseI64F64 = parserFixPoint(64, 64);
+
 /// map and reduce helper
 function mapReduce (arr, mapperFn, reducerFn) {
   let result = arr;
