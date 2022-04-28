0091e86c0f5049a48911f380dc82254b5daef061
Christian Langenbacher <clangenb+gh@protonmail.ch>
2022-04-28 11:45:55 -0400

    fix comment

--- src/Map.js
+++ src/Map.js
@@ -187,7 +187,7 @@ export default function Map (props) {
 
     debug && console.log('FETCHING LOCATIONS AND PROPERTIES');
 
-    // blockProductionTime == minimumPeriod * 2 as long as we use aura.
+    // blockProductionTime = minimumPeriod * 2 as long as we use aura.
     const blockProductionTime = constants.timestamp.minimumPeriod * 2 / 1_000;
 
     debug && console.log(`Timestamp.minimumPeriod: ${constants.timestamp.minimumPeriod}`);
