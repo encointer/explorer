8cde566406451ff8a6e19b2a744759ec060910e2
jaforrer <jaforrer@student.ethz.ch>
2022-10-22 11:22:38 +0200

    GetTentativeGrowth and GetNextMeetupDate are in the Lib file now

--- src/map/MapSidebar.js
+++ src/map/MapSidebar.js
@@ -196,8 +196,6 @@ function MapSidebarMain (props) {
 
       setIpfsUrl('https://ipfs.io/ipfs/' + ipfsCid + '/community_icon.svg');
     }
-    Printfunnystuff(api);
-    debug && console.log("this finally works! yaaaaaayyy"+ getNextMeetupDate(api, cid))
     getCommunityLogo();
   }, [api, cid]);
 
