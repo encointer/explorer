56d20c3f22bb45f37f10777e80f5c029a1e12810
jaforrer <jaforrer@student.ethz.ch>
2022-11-20 18:18:46 +0100

    initial commit. Now for sure

--- src/map/communityBusiness.js
+++ src/map/communityBusiness.js
@@ -2,37 +2,35 @@ import React from 'react';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Switch from '@material-ui/core/Switch';
 
-
-
 const CommunityBusinessSwitch = (props) => {
-    // Our States
-    const [state, setState] = React.useState({ status: true });
+  // Our States
+  const [state, setState] = React.useState({ status: true });
 
-    // Change State Function
-    const handleChange = (event) => {
-        setState({ ...state, [event.target.name]: event.target.checked });
-    };
+  // Change State Function
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+  };
 
-    return (
-        <div style={{
-            margin: 'auto',
-            display: 'block',
-            width: 'fit-content'
-        }}>
-            <h3>How to use Switch Component in ReactJS?</h3>
-            <FormControlLabel
-                control={
-                    <Switch
-                        checked={state.status}
-                        onChange={handleChange}
-                        color="primary"
-                        name="status"
-                    />
-                }
-                label="Switch Silent Mode"
-            />
-        </div>
-    );
-}
+  return (
+      <div style={{
+        margin: 'auto',
+        display: 'block',
+        width: 'fit-content'
+      }}>
+          <h3>How to use Switch Component in ReactJS?</h3>
+          <FormControlLabel
+              control={
+                   <Switch
+                      checked={state.status}
+                      onChange={handleChange}
+                      color="primary"
+                      name="status"
+                  />
+              }
+              label="Switch Silent Mode"
+          />
+      </div>
+  );
+};
 
 export default CommunityBusinessSwitch;
