40b1344f398f2d1bd3d55e199c6fccc599c2ce8f
jaforrer <jaforrer@student.ethz.ch>
2023-01-17 15:47:23 +0100

    Revert "Don't know how to solve the duplicate branch problem, but I imported the new function now"
    This reverts commit 23e3b065a2ed70383296534a9a7de5fd2d94e21e.

--- src/Lib.js
+++ src/Lib.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { locationFromJson, ipfsCidFromHex } from './utils';
 import { getNextMeetupTime, getCeremonyIncome } from '@encointer/node-api';
 import { parseI64F64 } from '@encointer/util';
-import { Header } from 'semantic-ui-react';
 
 const apiReady = (api, queryName = '') => {
   const query = api && api.queryMulti && api.query;
@@ -200,25 +199,20 @@ function getAssignedParticipantsComponent (ceremonyRegistry) {
   </div>);
 }
 
-export function ShowNumberOfSubmittedAttesters (props) {
-  const { api, cid } = props;
-  const [submittedAttesters, setSubmittedAttesters] = useState([]);
+const [submittedAttesters, setSubmittedAttesters] = useState([]);
 
-  useEffect(() => {
-    async function getNumberOfSubmittedAttesters () {
-      const CommunityCeremony = api.registry.getOrUnknown('CommunityCeremony');
-      const currentCeremonyIndex = await api.query.encointerScheduler.currentCeremonyIndex();
-      const currentCommunityCeremony = new CommunityCeremony(api.registry, [cid, currentCeremonyIndex]);
-      const numberOfSubmittedAttesters = await api.query.encointerCeremonies.attestationCount(currentCommunityCeremony);
-      setSubmittedAttesters(numberOfSubmittedAttesters.toNumber());
-    }
-    getNumberOfSubmittedAttesters();
-  }, [api, cid]);
+useEffect(() => {
+  async function getNumberOfSubmittedAttesters () {
+    const CommunityCeremony = api.registry.getOrUnknown('CommunityCeremony');
+    const currentCeremonyIndex = await api.query.encointerScheduler.currentCeremonyIndex();
+    const currentCommunityCeremony = new CommunityCeremony(api.registry, [cid, currentCeremonyIndex]);
+    const numberOfSubmittedAttesters = await api.query.encointerCeremonies.attestationCount(currentCommunityCeremony);
+    setSubmittedAttesters(numberOfSubmittedAttesters.toNumber());
+  }
+  getNumberOfSubmittedAttesters();
+}, [api, cid]);
 
-  return showNumberOfSubmittedAttesters(submittedAttesters, setSubmittedAttesters);
-}
-
-function showNumberOfSubmittedAttesters (submittedAttesters, setSubmittedAttesters) {
+function showNumberOfSubmittedAttesters () {
   if (submittedAttesters === null) {
     setSubmittedAttesters(0);
   }
