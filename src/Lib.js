4ac52d3254dfaf42c7857a28cc5dc1babed3e177
jaforrer <jaforrer@student.ethz.ch>
2023-01-17 15:48:17 +0100

    Revert "GetTentativeGrowth and GetNextMeetupDate are in the Lib file now"
    This reverts commit 7d972b6ec5a85c27128bc3bf7dec52bb88df4e77.

--- src/Lib.js
+++ src/Lib.js
@@ -1,20 +1,27 @@
-import { useEffect, useState } from 'react';
-import { locationFromJson } from './utils';
-import { getNextMeetupTime } from '@encointer/node-api';
+import React,{useEffect, useState} from "react";
+import { locationFromJson } from "./utils";
+import { getNextMeetupTime } from "@encointer/node-api";
 
 const apiReady = (api, queryName = '') => {
-  const query = api && api.queryMulti && api.query;
-  return query && queryName ? (!!query[queryName]) : !!query;
+    const query = api && api.queryMulti && api.query;
+    return query && queryName ? (!!query[queryName]) : !!query;
 };
 const formatDate = (timestamp) => (new Date(timestamp)).toLocaleString();
 
-export function GetNextMeetupDate (props) {
-  const { api, cid } = props;
-  const [nextMeetupTime, setNextMeetupTime] = useState([]);
+
+
+
+
+
+export function getNextMeetupDate(api, cid){
+
+  
+    const [nextMeetupTime, setNextMeetupTime] = useState([]);
   if (!apiReady(api, 'encointerScheduler')) {
     return;
   }
   useEffect(() => {
+    
     async function getNextMeetupDate () {
       const meetupLocations = await api.rpc.encointer.getLocations(cid);
       const tempLocation = locationFromJson(api, meetupLocations[0]);
@@ -22,45 +29,66 @@ export function GetNextMeetupDate (props) {
       setNextMeetupTime(formatDate(tempTime.toNumber()).split(',')[0]);
     }
     getNextMeetupDate();
-  }, [api, cid]);
-
-  return nextMeetupTime;
+      },[api,cid]);
+  
+return nextMeetupTime;
 }
 
-/**
-* Gets the relative tentative growth based on the current meetups registrations.
-* Returns the max allowed growth if the number of registered newbies exceeds the allowed newbie seats.
-*/
-export function GetTentativeGrowth (props) {
-  const { allReputableNumber, api, cid } = props;
-  const [tentativeGrowth, setTentativeGrowth] = useState([]);
-  if (!apiReady(api, 'encointerScheduler')) {
-    return;
-  }
-  useEffect(() => {
-    async function getTentativeGrowth (allReputableNumber) {
-      if (!allReputableNumber || allReputableNumber === 0) {
-        return 0;
-      }
 
-      const CommunityCeremony = api.registry.getOrUnknown('CommunityCeremony');
+//const { api, apiState, socket } = useSubstrate();
+
+export function Printfunnystuff(api){
+  if(apiReady(api, 'encointerScheduler')){
+    console.log("this thing works as it is planned")
+  }
+  
+  console.log("ldafjksjffjfjfjfjfjfj heeeeeeeeeeyyyyyyyyyyyyyyyyyyyyyyyaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa!!");
+  
+}
+export function doreturn(){
+  
+  return 2;
+}
+
+  //useEffect(() => {
+    /**
+     * Gets the relative tentative growth based on the current meetups registrations.
+     * Returns the max allowed growth if the number of registered newbies exceeds the allowed newbie seats.
+     */
+    /*async function getTentativeGrowth (allReputableNumber) {
       const meetupNewbieLimitDivider = api.consts.encointerCeremonies.meetupNewbieLimitDivider;
       const currentCeremonyIndex = await api.query.encointerScheduler.currentCeremonyIndex();
       const currentCommunityCeremony = new CommunityCeremony(api.registry, [cid, currentCeremonyIndex]);
       const newbies = await api.query.encointerCeremonies.newbieCount(currentCommunityCeremony);
-
+      
       const maxGrowthAbsolute = Math.min(
         newbies,
         Math.floor(allReputableNumber / meetupNewbieLimitDivider)
       );
 
       // round to 2 digits
-      return Math.round(maxGrowthAbsolute / allReputableNumber * 100) / 100;
+      return (allReputableNumber != null) ? Math.round(maxGrowthAbsolute / allReputableNumber * 100) / 100 : null;
     }
+    let isMounted = true;
+    if (allReputableNumber) {
+      getTentativeGrowth(allReputableNumber).then(data => {
+        if (isMounted) setTentativeGrowth(data);
+      });
+      return () => { isMounted = false; };
+    }
+    }   , []);*/
 
-    getTentativeGrowth(allReputableNumber).then(data => {
-      setTentativeGrowth(data);
-    });
-  }, [allReputableNumber, api, cid]);
-  return tentativeGrowth;
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
